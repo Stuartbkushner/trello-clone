e7f53ae8e2b09f2e42c49cee9e456ef2d4d14dd8
Stuart Kushner <stuartbkushner@gmail.com>
2020-11-24 09:17:51 -0500

    Rendered everything together

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
-
+import { Card } from './Card';
+import { Column } from './Column';
 import { AppContainer } from './styles';
 
 const App = () => {
   return (
     <AppContainer>
-      Columns Will Go Here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
     </AppContainer>
   )
 }
