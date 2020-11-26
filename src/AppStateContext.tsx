593f8a0043a7abf78718c9d34b65749e78451b1a
Stuart Kushner <stuartbkushner@gmail.com>
2020-11-26 15:17:29 -0500

    Can now add state

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,7 +1,9 @@
-import React, { createContext, useReducer, useContext } from "react"
+import React, { createContext, useReducer, useContext, useEffect } from "react"
 import { nanoid } from "nanoid"
 import { findItemIndexById, insertItemAtIndex, moveItem, overrideItemAtIndex, removeItemAtIndex } from "./utils/arrayUtils"
 import { DragItem } from "./DragItem"
+import { withData } from "./withData"
+import { save } from "./api"
 
 interface Task {
   id: string
@@ -162,36 +164,21 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
   }
 }
 
-const appData: AppState = {
-  draggedItem: undefined,
-  lists: [
-    {
-      id: "0",
-      text: "To Do",
-      tasks: [{ id: "c0", text: "Generate app scaffold" }]
-    },
-    {
-      id: "1",
-      text: "In Progress",
-      tasks: [{ id: "c2", text: "Learn Typescript" }]
-    },
-    {
-      id: "2",
-      text: "Done",
-      tasks: [{ id: "c3", text: "Begin to use static typing" }]
-    }
-  ]
-}
+export const AppStateProvider = withData(({ children, initialState }: React.PropsWithChildren<{initialState: AppState}>) => {
+  const [state, dispatch] = useReducer(appStateReducer, initialState)
 
-export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
-  const [state, dispatch] = useReducer(appStateReducer, appData)
+  useEffect(() => {
+    save(state)
+  }, [state])
 
   return (
-    <AppStateContext.Provider value={{ state, dispatch }}>
+    <AppStateContext.Provider
+      value={{ state, dispatch }}
+    >
       {children}
     </AppStateContext.Provider>
   )
-}
+})
 
 export const useAppState = () => {
   return useContext(AppStateContext)
