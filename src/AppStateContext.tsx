528c945db5c360e1997bd94cc0a3d1c375e646d9
Stuart Kushner <stuartbkushner@gmail.com>
2020-11-26 14:40:57 -0500

    Can now move cards around

--- src/AppStateContext.tsx
+++ src/AppStateContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer, useContext } from "react"
 import { nanoid } from "nanoid"
-import { findItemIndexById, overrideItemAtIndex, moveItem } from "./utils/arrayUtils"
+import { findItemIndexById, insertItemAtIndex, moveItem, overrideItemAtIndex, removeItemAtIndex } from "./utils/arrayUtils"
 import { DragItem } from "./DragItem"
 
 interface Task {
@@ -15,8 +15,8 @@ interface List {
 }
 
 export interface AppState {
+  draggedItem: DragItem | undefined
   lists: List[]
-  draggedItem: DragItem | undefined;
 }
 
 type Action =
@@ -39,13 +39,24 @@ type Action =
         hoverIndex: number
       }
     }
+  | {
+      type: "MOVE_TASK"
+      payload: {
+        dragIndex: number
+        hoverIndex: number
+        sourceColumn: string
+        targetColumn: string
+      }
+    }
 
 interface AppStateContextProps {
   state: AppState
   dispatch: React.Dispatch<Action>
 }
 
-const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+)
 
 const appStateReducer = (state: AppState, action: Action): AppState => {
   switch (action.type) {
@@ -93,13 +104,65 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
         lists: moveItem(state.lists, dragIndex, hoverIndex)
       }
     }
+    case "MOVE_TASK": {
+      const {
+        dragIndex,
+        hoverIndex,
+        sourceColumn,
+        targetColumn
+      } = action.payload
+
+      const sourceListIndex = findItemIndexById(
+        state.lists,
+        sourceColumn
+      )
+
+      const targetListIndex = findItemIndexById(
+        state.lists,
+        targetColumn
+      )
+
+      const sourceList = state.lists[sourceListIndex]
+      const task = sourceList.tasks[dragIndex]
+
+      const updatedSourceList = {
+        ...sourceList,
+        tasks: removeItemAtIndex(sourceList.tasks, dragIndex)
+      }
+
+      const stateWithUpdatedSourceList = {
+        ...state,
+        lists: overrideItemAtIndex(
+          state.lists,
+          updatedSourceList,
+          sourceListIndex
+        )
+      }
+
+      const targetList =
+        stateWithUpdatedSourceList.lists[targetListIndex]
+
+      const updatedTargetList = {
+        ...targetList,
+        tasks: insertItemAtIndex(targetList.tasks, task, hoverIndex)
+      }
+
+      return {
+        ...stateWithUpdatedSourceList,
+        lists: overrideItemAtIndex(
+          stateWithUpdatedSourceList.lists,
+          updatedTargetList,
+          targetListIndex
+        )
+      }
+    }
     default: {
       return state
     }
   }
 }
 
-const appData: AppState= {
+const appData: AppState = {
   draggedItem: undefined,
   lists: [
     {
--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -1,9 +1,13 @@
-interface Item {
-  id: string
+export function removeItemAtIndex<T>(array: T[], index: number) {
+  return [...array.slice(0, index), ...array.slice(index + 1)]
 }
 
-export const findItemIndexById = <T extends Item>(items: T[], id: string) => {
-  return items.findIndex((item: T) => item.id === id)
+export function insertItemAtIndex<T>(
+  array: T[],
+  item: T,
+  index: number
+) {
+  return [...array.slice(0, index), item, ...array.slice(index)]
 }
 
 export function overrideItemAtIndex<T>(
@@ -20,19 +24,15 @@ export function overrideItemAtIndex<T>(
   })
 }
 
+interface Item {
+  id: string
+}
+
+export const findItemIndexById = <T extends Item>(items: T[], id: string) => {
+  return items.findIndex((item: T) => item.id === id)
+}
+
 export const moveItem = <T>(array: T[], from: number, to: number) => {
   const item = array[from]
   return insertItemAtIndex(removeItemAtIndex(array, from), item, to)
-}
-
-export function removeItemAtIndex<T>(array: T[], index: number) {
-  return [...array.slice(0, index), ...array.slice(index + 1)]
-}
-
-export function insertItemAtIndex<T>(
-  array: T[],
-  item: T,
-  index: number
-) {
-  return [...array.slice(0, index), item, ...array.slice(index)]
 }
